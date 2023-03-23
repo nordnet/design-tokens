3d3ffec2f23f1b267880bd8de371026f8548e494
Mansour Mansour <mansour@mjmonline.se>
2023-03-23 16:44:37 +0100

    fix: simplify theme handling

--- transformTokens.js
+++ transformTokens.js
@@ -1,43 +1,38 @@
 const StyleDictionary = require("style-dictionary");
-const assert = require("node:assert");
 const { getConfig } = require("./config");
 
 const { fileHeader } = StyleDictionary.formatHelpers;
 
-const args = process.argv.slice(2).reduce((acc, pairStr) => {
-  const [_key, value] = pairStr.split("=");
-  const key = _key.replace("--", "");
-  return { ...acc, [key]: value };
-}, {});
+const supportedThemes = ["light", "dark", "accessibility"];
 
-assert(args.theme, 'Must provide "--theme" arg');
-const baseConfig = getConfig(args.theme);
+supportedThemes.map((theme) => {
+  console.log(`🤖 Compiling tokens for the ${theme.toUpperCase()} theme`);
 
-console.log(`🤖 Compiling tokens for the ${args.theme.toUpperCase()} theme`);
+  StyleDictionary.registerParser({
+    pattern: /\.json$/,
+    parse: ({ contents }) => {
+      return {
+        color: JSON.parse(contents).color[theme],
+      };
+    },
+  });
 
-StyleDictionary.registerParser({
-  pattern: /\.json$/,
-  parse: ({ contents }) => {
-    return {
-      color: JSON.parse(contents).color[args.theme],
-    };
-  },
+  StyleDictionary.registerFormat({
+    name: "custom/typescript/es6-declarations",
+    formatter: function ({ dictionary, file }) {
+      return (
+        fileHeader({ file }) +
+        dictionary.allTokens
+          .map(function (token) {
+            return `export declare const ${token.name}: string;`;
+          })
+          .join("\n")
+      );
+    },
+  });
+
+  const config = getConfig(theme);
+  const StyleDictionaryExtended = StyleDictionary.extend(config);
+
+  StyleDictionaryExtended.buildAllPlatforms();
 });
-
-StyleDictionary.registerFormat({
-  name: "custom/typescript/es6-declarations",
-  formatter: function ({ dictionary, file }) {
-    return (
-      fileHeader({ file }) +
-      dictionary.allTokens
-        .map(function (token) {
-          return `export declare const ${token.name}: string;`;
-        })
-        .join("\n")
-    );
-  },
-});
-
-const StyleDictionaryExtended = StyleDictionary.extend(baseConfig);
-
-StyleDictionaryExtended.buildAllPlatforms();
