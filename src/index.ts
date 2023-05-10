af7fa67a04c1fda629ce1712d3791397f082e673
Mansour Mansour <mansour@mjmonline.se>
2023-05-10 15:01:17 +0200

    fix: export a11yTheme correctly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { LightTheme, colors as lightTheme } from "./generated/js/lightTheme";
 import { DarkTheme, colors as darkTheme } from "./generated/js/darkTheme";
-import { A11yTheme, colors as a11yThemeTheme } from "./generated/js/a11yTheme";
+import { A11yTheme, colors as a11yTheme } from "./generated/js/a11yTheme";
 
 export type { LightTheme, DarkTheme, A11yTheme };
-export { lightTheme, darkTheme, a11yThemeTheme };
+export { lightTheme, darkTheme, a11yTheme };
